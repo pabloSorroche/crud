f1c81ac29b91b48252a98c78308a339236ec031f
Pablo <pablo.sorroche.dev@gmail.com>
2025-05-28 17:22:02 +0200

    Feat: remove logs

--- src/contexts/auth/infrastructure/middlewares/authorizeRoles.ts
+++ src/contexts/auth/infrastructure/middlewares/authorizeRoles.ts
@@ -6,14 +6,10 @@ export const authorizeRoles =
   (allowedRoles: UserRole[]) => (_: Request, res: Response, next: NextFunction) => {
     const user = res.locals.user;
 
-    console.log(user);
-
     if (!user) {
       res.status(401).json({ error: 'Not authenticated ' });
       return;
     }
-
-    console.log({ user });
 
     if (!allowedRoles.includes(user.role)) {
       res.status(403).json({ error: 'Access denied' });
